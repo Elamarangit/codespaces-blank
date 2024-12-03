a576bbb69a3b8ed6ccbd5ef8b8813c31f8a7e1d1
Elamarangit <ee8772@pleasantonusd.net>
2024-12-03 07:17:01 -0800

    Update PlayerStatsManager.java

--- PlayerStatsManager.java
+++ PlayerStatsManager.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 
 public class PlayerStatsManager {
-    // Classifies what an PlayerStats object should contain
 
 
-    private List<PlayerStat> playerData = PlayerStatsDownloader.readCsvFromUrl("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/NBA_2021.csv");
 
-    public void normalStats(String nameInput) {
+    private List<PlayerStat> playerData = PlayerStatsDownloader.readCsvFromUrl("NBA_2021.csv");
+    public static boolean playerFound = false;
+
+    public void  advancedStats(String nameInput) {
+        playerFound = false;
         boolean detected = false;
         PlayerStat detectedPlayer = null;
         for (PlayerStat playerStat:playerData) {
@@ -21,9 +23,35 @@ public class PlayerStatsManager {
         }
         if (detected != true) {
             System.out.println("I'm sorry but this player's name isn't available on our database. This is possible because it only contains player carrer data per the 2021 season");
+            playerFound = true;
+  
         }
         if (detectedPlayer != null) {
-            print(detectedPlayer);
+            printAdvanced(detectedPlayer);
+
+
+        }
+
+ 
+    }
+
+    public void normalStats(String nameInput) {
+        playerFound = false;
+        boolean detected = false;
+        PlayerStat detectedPlayer = null;
+        for (PlayerStat playerStat:playerData) {
+            if(playerStat.getPlayerName().equalsIgnoreCase(nameInput)) {
+                detected = true;
+                detectedPlayer = playerStat;
+                break;
+            }
+        }
+        if (detected != true) {
+            System.out.println("I'm sorry but this player's name isn't available on our database. This is possible because it only contains player carrer data per the 2021 season");
+            playerFound = true;
+        }
+        if (detectedPlayer != null) {
+            printNormal(detectedPlayer);
 
 
         }
@@ -59,8 +87,8 @@ public class PlayerStatsManager {
             detectedAB = true;
         }
         if (detectedAB) {
-            print(detectedPlayerA);
-            print(detectedPlayerB);
+            printAdvanced(detectedPlayerA);
+            printAdvanced(detectedPlayerB);
             if (detectedPlayerA.getPoints() > detectedPlayerB.getPoints()) {
                 ScoreA += 1;
             }
@@ -111,26 +139,30 @@ public class PlayerStatsManager {
             }
         }
         if (ScoreA > ScoreB) {
+            System.out.println("");
+            System.err.println("");
             System.out.println(Aplayer + " is a statistically better player than "+ Bplayer + " because he is a better overall player considering the categories scoring, rebounding, playmaking, defense and efficiency");
         }
         if (ScoreB > ScoreA) {
+            System.out.println("");
+            System.out.println("");
             System.out.println(Bplayer + " is a statistically better player than "+ Aplayer + " because he is a better overall player considering the categories scoring, rebounding, playmaking, defense and efficiency");
         }
         if (ScoreA == ScoreB && detectedAB) {
-            System.out.println(Aplayer + " is a statistically as good as "+ Bplayer + " because they are the same overall player considering the categories scoring, rebounding, playmaking, defense and efficiency");
+            System.out.println("");
+            System.out.println("");
+            System.out.println(Aplayer + " is a statistically as good as "+ Bplayer + " because they are almost the same overall player considering the categories scoring, rebounding, playmaking, defense and efficiency");
         }
 
 
     }
 
 
-    public void advancedStats(String nameInput) {
-        // takes in the player's name, refers ro the API return and then it gives out the 2D array
-        // The returned array has 7 collumns
-    }
 
-    private void print(PlayerStat detectedPlayer) {
-        System.out.println("Here are the normal stats of "+detectedPlayer.getPlayerName());
+    private void printAdvanced(PlayerStat detectedPlayer) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Here are the advanced stats of "+detectedPlayer.getPlayerName());
         System.out.println("------------");
         System.out.println("Age: " + detectedPlayer.getAge());
         System.out.println("Position: " + detectedPlayer.getPosition());
@@ -143,4 +175,22 @@ public class PlayerStatsManager {
         System.out.println("Blocks per game: " + detectedPlayer.getBlocks());
         System.out.println("Turnovers per game: " + detectedPlayer.getTurnovers());
     }
+
+    private void printNormal(PlayerStat detectedPlayer) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Here are the normal stats of "+detectedPlayer.getPlayerName());
+        System.out.println("------------");
+        System.out.println("Age: " + detectedPlayer.getAge());
+        System.out.println("Position: " + detectedPlayer.getPosition());
+        System.out.println("Points per game: " + detectedPlayer.getPoints());
+        System.out.println("Rebounds per game: " + detectedPlayer.getRebound());
+        System.out.println("Assists per game: " + detectedPlayer.getAssists());
+    }
+
+    public static boolean isPlayerFound() {
+        return playerFound;
+    }
+
+
 }
